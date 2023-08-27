20e25f1e449ed7978792cbebebba0a8723c68a88
Matthias Rieck <Matthias.Rieck@icloud.com>
2023-08-27 11:20:27 +0200

    Selection Form

--- logitfy/logitfy-app/src/App.tsx
+++ logitfy/logitfy-app/src/App.tsx
@@ -1,33 +1,78 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import './ui/styles.css'
+
+import * as Ariakit from "@ariakit/react";
+
+import { useDeferredValue, useMemo, useState } from "react";
+import { matchSorter } from "match-sorter";
+import { Combobox, ComboboxItem } from "./ui/combobox-multiple.js";
+import Submodule from './ui/selected-submodule.tsx';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [submoduleNames, setSubmoduleNames] = useState<string[]>([])
+  const [value, setValue] = useState("");
+  const [values, setValues] = useState<string[]>([]);
+  const deferredValue = useDeferredValue(value);
+
+  const matches = useMemo(
+    () => matchSorter(submoduleNames, deferredValue),
+    [deferredValue],
+  );
+
+  function onSubmoduleClick(value: string) {
+    setValues(values.filter((x) => x != value))
+  }
 
   return (
     <>
+      <header>
+        <h1>LOGITFY - Changelog as a Service</h1>
+        <h2>Select the root repository</h2>
+        <div className='selection_row'>
+          <input className='input' placeholder='owner/repo'></input>
+          <Ariakit.Button className="button">Select Root Repository</Ariakit.Button>
+        </div>
+        {
+          submoduleNames.length > 0 &&
+          <>
+            <h2>Select Submodules</h2>
+            {
+              values.length > 0 && <div className='row_submodules'>
+                {values.map((val: string) => <Submodule key={val} name={val} onClick={onSubmoduleClick} />)}
+              </div>
+            }
+            <div className='selection_row'>
+
+              <Combobox
+                placeholder="search submodules"
+                value={value}
+                onChange={setValue}
+                values={values}
+                onValuesChange={setValues}
+              >
+                {matches.map((value, i) => (
+                  <ComboboxItem key={value + i} value={value} />
+                ))}
+                {!matches.length && <div className="no-results">No results found</div>}
+              </Combobox>
+            </div>
+          </>
+        }
+        <h2> Select start (sha, tag) and end ref (sha, tag, branch)</h2>
+        <div className='selection_row'>
+          <input className='input' placeholder='start-ref'></input>
+          &rarr;
+          <input className='input' placeholder='end-ref'></input>
+          <Ariakit.Button className="button"><b>Submit</b></Ariakit.Button>
+        </div>
+      </header >
+
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+
     </>
   )
 }
